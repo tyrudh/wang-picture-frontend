cd173b5ecda0f113ed8d31eb9903794a41661456
trudh <3373807551@qq.com>
2025-09-05 22:55:08 +0800

    前端初始化

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="globalHeader">
+    <a-row :wrap="false">
+      <a-col flex="200px">
+        <router-link to="/">
+          <div class="title-bar">
+            <img class="logo" src="../assets/logo.png" alt="logo" />
+            <div class="title">文具盒图库</div>
+          </div>
+        </router-link>
+      </a-col>
+      <a-col flex="auto">
+        <a-menu
+          v-model:selectedKeys="current"
+          mode="horizontal"
+          :items="items"
+          @click="doMenuClick"
+        />
+      </a-col>
+      <a-col flex="120px">
+        <div class="user-login-status">
+          <a-button type="primary" href="/user/login">登录</a-button>
+        </div>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+<script lang="ts" setup>
+import { h, ref } from 'vue'
+import { HomeOutlined } from '@ant-design/icons-vue'
+import { MenuProps } from 'ant-design-vue'
+import HomeView from '@/views/HomeView.vue'
+import { useRouter } from 'vue-router'
+
+const items = ref<MenuProps['items']>([
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: '主页',
+    title: '主页',
+  },
+  {
+    key: '/about',
+    label: '关于',
+    title: '关于',
+  },
+  {
+    key: 'others',
+    label: h('a', { href: 'https://www.bilibili.com/', target: '_blank' }, '视频网站'),
+  },
+])
+const router = useRouter()
+//路由跳转函数
+const doMenuClick = ({ key }) => {
+  router.push({
+    path: key,
+  })
+}
+//当前高亮的菜单项
+const current = ref<string[]>([])
+//如何获取到当前的url从router中获取的办法
+router.afterEach((to, from, next) => {
+  current.value = [to.path]
+})
+</script>
+<style scoped>
+#globalHeader .title-bar {
+  display: flex;
+  align-items: center;
+}
+.title {
+  color: black;
+  font-size: 20px;
+  margin-left: 16px;
+}
+.logo {
+  width: 25px;
+  height: 25px;
+}
+</style>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+        <GlobalHeader></GlobalHeader>
+      </a-layout-header>
+      <a-layout-content class="content">Content
+      <router-view></router-view>
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a target="_blank">图库网站</a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
+
+<style scoped>
+#basicLayout .footer{
+  color: aqua;
+  padding: 70px;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  text-align: center;
+  font-size: 20px;
+}
+#basicLayout .content{
+  padding: 16px;
+  background: beige;
+  margin-bottom: 20px;
+}
+#basicLayout .header{
+  padding-inline: 20px;
+  background: white;
+  color: unset;
+  margin-bottom: 16px;
+}
+</style>
